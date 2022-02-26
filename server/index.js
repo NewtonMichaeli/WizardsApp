f0410e0d17f971b95f24416148e6ab92eabc26af
Lior Bedok <shadowcyanil@gmail.com>
2022-02-26 17:06:10 +0200

    wizards route reference added in index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const authRoute = require('./routes/authRoute')
+const wizardRoute = require('./routes/wizardRoutes')
 const cors = require('cors')
 
 const app = express()
@@ -13,5 +14,6 @@ app.use(express.urlencoded({extended: false}))
 
 //app routes
 app.use('/api/auth', authRoute)
+app.use('/api/wizards', wizardRoute)
 
 app.listen({PORT} = process.env, () => console.log(`listening on port ${PORT}`))
